bb73158af6f230440803c3d9365e8c9d681ff02f
Brenner L. Oliveira <brennerlimaoliveira@gmail.com>
2019-08-13 18:48:11 -0300

    Configura o redux para o carrinho

--- src/index.js
+++ src/index.js
@@ -1,13 +1,16 @@
 import React from 'react';
 import { StatusBar } from 'react-native';
+import { Provider } from 'react-redux';
 
 import Routes from './routes';
 
+import store from './store';
+
 export default function App() {
   return (
-    <>
+    <Provider store={store}>
       <StatusBar barStyle="light-content" backgroundColor="#191920" />
       <Routes />
-    </>
+    </Provider>
   );
 }
